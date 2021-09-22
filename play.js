cc12cdc67dff7f5c4da10328299a49a7d72062b6
Josh Millard <josh.millard10@gmail.com>
2021-09-22 14:32:52 -0400

    escape command implimented and stdin input setup

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 const net = require("net");
 const connect = require('./client')
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  if ('\u0003') {
+    process.exit();
+  }
+}
+
+
+
 console.log("Connecting ...");
 connect();
+setupInput()
